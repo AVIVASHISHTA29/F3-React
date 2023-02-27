13f5f4fa2bf885ca124065a20eeddc50807e5e76
avivashishta29 <avivashishta@gmail.com>
2023-02-27 21:19:15 +0530

    onClick and onChange

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <Footer />
       <MainComponent />
+      <Footer />
     </div>
   );
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,37 @@
 import "./styles.css";
+
 function MainComponent() {
-  return <div>This is my MainComponent</div>;
+  const myName = "Avi";
+  const flag = false;
+
+  // function myFunction(name) {
+  //   console.log("hi " + name);
+  // }
+
+  const myFunction = () => {
+    sum = num1 + num2;
+  };
+
+  // var my_name vs var myName
+
+  return (
+    <div className="main-container">
+      <h1
+      // onClick={() => myFunction()}
+      // onMouseDown={() => {
+      //   console.log("mouse is clicking down on h1");
+      // }}
+      // onMouseUp={() => {
+      //   console.log("mouse is clicking up on h1");
+      // }}
+      // onMouseOver={() => console.log("mouse is over the h1")}
+      >
+        {sum}
+      </h1>
+      <input type="text" onChange={(e) => console.log(e.target.value)} />
+      {/* <button onclick="myFunction()">My button</button> */}
+      <button onClick={() => myFunction()}>My button</button>
+    </div>
+  );
 }
 export default MainComponent;
